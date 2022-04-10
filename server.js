011a974d993e38ed2b3695e631d7bba1252fa632
Tran Dac Nguyen Kim <trandacnguyenkim@Trans-MacBook-Pro.local>
2022-04-10 11:21:24 -0400

    more route created

--- server.js
+++ server.js
@@ -10,42 +10,49 @@ const fs = require("fs");
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
+// The index route
+app.get('/', function(req, res) {
+    res.send('Cloud Raindrop Score Board');
+ });
 
-//add new score to json
-function addNewScore(newScore){
+//add score route
+app.post("/uploadscore", async function(req, res){
+    //get score
+    let {name, score} = req.body;
+    //create new player
     var readScores = fs.readFileSync('scores.json');
     var myObject = JSON.parse(readScores);
-    myObject.push(newScore);
+    myObject.push({name, score});
     var newData = JSON.stringify(myObject, null, 2);
     fs.writeFile('scores.json', newData, err => {
         // error checking
-        if(err) throw err;
-        
+        if(err) {
+            res.send({status:false, msg:"error occur"})
+            throw err;
+        }
         console.log("New score added");
+        res.send({status: true, msg: "new score added"});
     });
-}
-
-let newPlayerScore = {
-    "name": "Kim",
-    "score": 200
-}
-addNewScore(newPlayerScore);
-
-//get top 3
-function getTopThree(){
-    const data = JSON.parse(fs.readFileSync("./scores.json", "utf8"));
-    sortedData = data.sort((a, b) => {
-        if (a.score < b.score) return -1;
-        if (a.score > b.score) return 1;
-        return 0;
-      }).reverse();
-      return sortedData[0], sortedData[1], sortedData[2];
-}
-
-app.get('/player', (req, res) => {
-    res.send(getTopThree());
 });
 
+//get previous score route
+app.get("/previousscore", async function(req, res){
+    const data = JSON.parse(fs.readFileSync("./scores.json", "utf8"));
+    const prevScore = data.filter(data => data.score = req.body.score);
+    console.log(prevScore);
+    res.send({status: true, msg: "previous score retrieved"});
+});
+
+//scoreboard route
+app.get("/scoreboard", async function(req, res){
+    const data = JSON.parse(fs.readFileSync("./scores.json", "utf8"));
+    var top3 = data.sort((a, b) => {
+        if (a.score < b.score) return -1;
+        if (a.score > b.score) return 1;
+      }).reverse().slice(0, 3)    
+    console.log(top3);
+    res.send({status: true, msg: "top 3 retrieved"});
+    });
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
